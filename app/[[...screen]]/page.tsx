0ba067f00ad36f11e5ec691f891e852624f0d505
stellabubble <rjnarakmakmak@gmail.com>
2026-09-10 18:20:40 +0700

    style: update PageElements, dynamic routing, and global CSS

--- app/[[...screen]]/page.tsx
+++ app/[[...screen]]/page.tsx
@@ -5,6 +5,7 @@ import CustomersScreen from "../_screens/CustomersScreen";
 import DashboardScreen from "../_screens/DashboardScreen";
 import EmployeesScreen from "../_screens/EmployeesScreen";
 import HistoryScreen from "../_screens/HistoryScreen";
+import InvenHistoryScreen from "../_screens/InvenHistoryScreen";
 import InventoryScreen from "../_screens/InventoryScreen";
 import LoginScreen from "../_screens/LoginScreen";
 import NotificationsScreen from "../_screens/NotificationsScreen";
@@ -18,6 +19,7 @@ const routeScreens: Record<string, ComponentType> = {
   employees: EmployeesScreen,
   history: HistoryScreen,
   inventory: InventoryScreen,
+  "inven-history": InvenHistoryScreen,
   login: LoginScreen,
   notifications: NotificationsScreen,
   pos: PosScreen,
